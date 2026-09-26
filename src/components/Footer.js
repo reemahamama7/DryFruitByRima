f093debe7ce8f75090d9b5da573022e35a62892a
reemahamama7 <48969432+reemahamama7@users.noreply.github.com>
2026-09-26 13:34:04 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 const Footer = () => {
   return (
